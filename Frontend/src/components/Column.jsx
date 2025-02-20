55ec77ac1f919434d7cb9a01fdfa8f00c43f7a04
Muhammad Ranju <mdranju23@gmail.com>
2025-02-20 20:32:28 +0600

    implement sortable context in Column component; update TaskCard props for delete and edit functionality

--- Frontend/src/components/Column.jsx
+++ Frontend/src/components/Column.jsx
@@ -1,26 +1,33 @@
 /* eslint-disable react/prop-types */
 import { useDroppable } from "@dnd-kit/core";
+import {
+  horizontalListSortingStrategy,
+  SortableContext,
+} from "@dnd-kit/sortable";
 import TaskCard from "./TaskCard";
 
-function Column({ column, tasks, handleTaskClick }) {
+function Column({ column, tasks, onDeleteTask, onEditTask }) {
   const { setNodeRef } = useDroppable({ id: column._id });
 
   return (
-    <div className="flex w-96 flex-col  bg-gray-800 p-5 rounded-lg shadow-md min-h-[300px]">
+    <div className="flex w-96 flex-col bg-gray-800 p-5 rounded-lg shadow-md min-h-[300px]">
       <h2 className="mb-4 font-semibold text-neutral-100">{column.title}</h2>
-      <div ref={setNodeRef} className="flex flex-1 flex-col gap-4 ">
-        {tasks.length > 0 ? (
-          tasks.map((task) => (
-            <TaskCard
-              key={task._id}
-              task={task}
-              handleTaskClick={handleTaskClick}
-            />
-          ))
-        ) : (
-          <p className="text-neutral-500 text-sm">No tasks</p>
-        )}
-      </div>
+      <SortableContext items={tasks} strategy={horizontalListSortingStrategy}>
+        <div ref={setNodeRef} className="flex flex-1 flex-col gap-4">
+          {tasks.length > 0 ? (
+            tasks.map((task) => (
+              <TaskCard
+                key={task._id}
+                task={task}
+                onDeleteTask={onDeleteTask}
+                onEditTask={onEditTask}
+              />
+            ))
+          ) : (
+            <p className="text-neutral-500 text-sm">No tasks</p>
+          )}
+        </div>
+      </SortableContext>
     </div>
   );
 }
