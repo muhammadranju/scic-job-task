0e5a756bb197e0c240b6357ea24b5ecffac99f0b
Muhammad Ranju <mdranju23@gmail.com>
2025-02-20 20:32:43 +0600

    refactor TaskCard component to use props for delete and edit functionality; enhance UI with hover effects

--- Frontend/src/components/TaskCard.jsx
+++ Frontend/src/components/TaskCard.jsx
@@ -1,33 +1,38 @@
 /* eslint-disable react/prop-types */
+
 import { useDraggable } from "@dnd-kit/core";
+
 import { useState } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
-function TaskCard({ task, handleTaskClick }) {
+function TaskCard({ task, onDeleteTask, onEditTask }) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: task._id,
   });
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [newTask, setNewTask] = useState({
-    title: "",
-    description: "",
-    status: "",
+  const [editedTask, setEditedTask] = useState({
+    title: task.title,
+    description: task.description,
+    status: task.status,
   });
+
+  const [mouseIsOver, setMouseIsOver] = useState(false);
 
   const style = transform
     ? { transform: `translate(${transform.x}px, ${transform.y}px)` }
     : undefined;
 
-  const onDelete = (task) => {
-    console.log("Deleting task:", task);
+  const handleDelete = () => {
+    setIsModalOpen(true);
+    onDeleteTask(task._id);
   };
 
-  const onEdit = (task) => {
-    console.log("Editing task:", task);
+  const handleEdit = () => {
     setIsModalOpen(true);
   };
 
-  const updateTask = async () => {
+  const handleUpdate = () => {
+    onEditTask(task._id, editedTask);
     setIsModalOpen(false);
   };
 
@@ -37,8 +42,9 @@ function TaskCard({ task, handleTaskClick }) {
         ref={setNodeRef}
         {...listeners}
         {...attributes}
-        onClick={() => handleTaskClick(task._id)} // Call the reorder function on click
-        className={`cursor-grab rounded-lg  p-4 shadow-sm bg-gray-700 hover:shadow-md transition-transform flex justify-between items-center ${
+        onMouseEnter={() => setMouseIsOver(true)}
+        onMouseLeave={() => setMouseIsOver(false)}
+        className={`cursor-grab rounded-lg p-4 shadow-sm bg-gray-700 hover:shadow-md transition-transform flex justify-between items-center ${
           (task.status === "DONE" && "bg-green-600/50") ||
           (task.status === "IN_PROGRESS" && "bg-yellow-600/50")
         }`}
@@ -55,47 +61,49 @@ function TaskCard({ task, handleTaskClick }) {
             {task.description}
           </p>
         </div>
-        <div className="flex gap-2 ">
-          <button onClick={onEdit} className="text-green-400">
-            <FaEdit />
-          </button>
-          <button onClick={() => onDelete(task.id)} className="text-red-400">
-            <FaTrash />
-          </button>
-        </div>
+        {mouseIsOver && (
+          <div className="flex gap-2">
+            <button onClick={handleEdit} className="text-green-400 p-2">
+              <FaEdit className="text-2xl" />
+            </button>
+            <button onClick={handleDelete} className="text-red-400 p-2">
+              <FaTrash className="text-xl" />
+            </button>
+          </div>
+        )}
       </div>
 
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
           <div className="bg-gray-800 p-6 rounded-lg w-96">
-            <h2 className="text-lg font-bold mb-4">Add Task</h2>
+            <h2 className="text-lg font-bold mb-4">Edit Task</h2>
             <input
               type="text"
               placeholder="Title"
               className="w-full p-2 mb-2 bg-gray-700 text-white rounded"
-              value={newTask.title}
+              value={editedTask.title}
               onChange={(e) =>
-                setNewTask({ ...newTask, title: e.target.value })
+                setEditedTask({ ...editedTask, title: e.target.value })
               }
             />
             <textarea
               placeholder="Description"
               className="w-full p-2 mb-2 bg-gray-700 text-white rounded"
-              value={newTask.description}
+              value={editedTask.description}
               onChange={(e) =>
-                setNewTask({ ...newTask, description: e.target.value })
+                setEditedTask({ ...editedTask, description: e.target.value })
               }
             ></textarea>
             <select
               className="w-full p-2 mb-2 bg-gray-700 text-white rounded"
-              value={newTask.category}
+              value={editedTask.status}
               onChange={(e) =>
-                setNewTask({ ...newTask, category: e.target.value })
+                setEditedTask({ ...editedTask, status: e.target.value })
               }
             >
-              <option value="To-Do">To-Do</option>
-              <option value="In Progress">In Progress</option>
-              <option value="Done">Done</option>
+              <option value="TODO">To-Do</option>
+              <option value="IN_PROGRESS">In Progress</option>
+              <option value="DONE">Done</option>
             </select>
             <div className="flex justify-end gap-2">
               <button
@@ -105,10 +113,10 @@ function TaskCard({ task, handleTaskClick }) {
                 Cancel
               </button>
               <button
-                onClick={updateTask}
+                onClick={handleUpdate}
                 className="px-4 py-2 bg-green-500 text-white rounded"
               >
-                Add
+                Update
               </button>
             </div>
           </div>
