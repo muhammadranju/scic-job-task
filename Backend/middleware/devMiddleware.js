03736ae109c5cbfd9d8360d2de23d7dfc5531add
Muhammad Ranju <mdranju23@gmail.com>
2025-02-21 08:16:58 +0600

    fix: update CORS origin to allow specific domains and add socket.io-client dependency

--- Backend/middleware/devMiddleware.js
+++ Backend/middleware/devMiddleware.js
@@ -5,7 +5,7 @@ const middleware = [
   express.json(),
   express.urlencoded({ extended: true }),
   cors({
-    origin: "*",
+    origin: "https://task-manager-9f8db.web.app" || "http://localhost:5173",
     credentials: true,
   }),
   morgan("dev"),
